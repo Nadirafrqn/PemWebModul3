3a2c711ccbc67f00da9b69fd0733647dc4526ef2
Nadirafrqn <127399944+Nadirafrqn@users.noreply.github.com>
2023-10-24 16:23:54 +0700

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Us Page</h2>
+      <p>Learn about our company.</p>
+    </div>
+  );
+}
+
+export default About;
